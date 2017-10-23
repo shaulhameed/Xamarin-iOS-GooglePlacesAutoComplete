d716b3c1964a776cc419fe61545004fc671b04fa
Shaul Hameed <shaulhameed.it@gmail.com>
2017-10-23 15:42:03 +0400

    initial commit

---
+++ GooglePlacesAutoComplete/ViewController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using UIKit;
+using Google.Maps;
+using Foundation;
+
+namespace GooglePlacesAutoComplete
+{
+    public partial class ViewController : UIViewController, IAutocompleteViewControllerDelegate
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void OnGooglePlacesClick(UIButton sender)
+        {
+            var autoCompleteVC = new AutocompleteViewController();
+            autoCompleteVC.Delegate = this;
+
+            this.PresentViewController(autoCompleteVC, false, null);
+        }
+
+        public void DidAutocomplete(AutocompleteViewController viewController, Place place)
+        {
+            Debug.Print(place.Name);
+            Debug.Print(place.FormattedAddress);
+            this.DismissViewController(false, null);
+        }
+
+        public void DidFailAutocomplete(AutocompleteViewController viewController, NSError error)
+        {
+
+        }
+
+        public void WasCancelled(AutocompleteViewController viewController)
+        {
+            this.DismissViewController(false, null);
+        }
+    }
+}
